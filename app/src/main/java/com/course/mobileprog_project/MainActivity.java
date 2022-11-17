5ad8aefb592d4202cb5be5bfebba6430d4c2d3ab
jehun0506 <jehunoo88@gmail.com>
2022-11-17 10:04:56 +0900

    test

--- app/src/main/java/com/course/mobileprog_project/MainActivity.java
+++ app/src/main/java/com/course/mobileprog_project/MainActivity.java
@@ -2,13 +2,16 @@ package com.course.mobileprog_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.course.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState); // built in contents
+        setContentView(R.layout.activity_main); // customize
     }
 }
